86c2f51d4d160b018ad022d55b074e5342d67182
AliPTehrani <ali.p.tehrani@gmail.com>
2021-04-09 10:32:19 +0200

    Commit 1 .
    Only python files with code

---
+++ generate_features.py
@@ -0,0 +1,87 @@
+import torch
+import numpy as np
+import os
+import torchvision.models as models
+from torchvision import transforms as trn
+from PIL import Image
+import torch.nn as nn
+from networks.alexnet import *
+from torch.autograd import Variable as V
+import glob
+import helper
+
+'''
+This file is used to generate the features of the Neural Network from all stimuli
+Functions:
+preprocess_image : preprocessing an image for the neural network
+get_images : get the path for all images
+run_alexnet : generate the features for all images
+
+'''
+
+
+def preprocess_image(image):
+    """function to resize images"""
+    centre_crop = trn.Compose([
+        trn.Resize((224, 224)),  # resize to 224 x 224 pixels
+        trn.ToTensor(),  # transform to tensor
+        trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # normalize according to ImageNet
+    ])
+    img = Image.open(image)
+    input_img = V(centre_crop(img).unsqueeze(0))  # apply resizing
+    if torch.cuda.is_available():
+        input_img = input_img.cuda()
+    return input_img
+
+
+def get_images():
+    """Return list of all stimuli"""
+    path = "stimuli_jpg"
+    images = glob.glob(path + "/**/*.jpg", recursive = True)
+    return images
+
+
+def run_torchvision_model(model, result_path):
+    """
+    This function is used to run the Neural Network and save the features as .npz files
+    Runs all torchvision architectures
+    """
+    if torch.cuda.is_available():
+        model.cuda()
+
+    model.eval()
+    #directory of results
+    images = get_images()
+
+    for image in images:
+
+        sep = helper.check_platform()  # depending on platform we have different seperators
+        filename = image.split(sep)[-1].split(".")[0]
+        filetype = image.split(sep)[-1].split(".")[1]
+        filefolder = image.split(sep)[0] + image.split(sep)[1]
+        net_save_dir = os.path.join(result_path, image.split(sep)[1])
+
+        if filetype == "jpg":
+            valid_type = True
+            input_img = preprocess_image(image)  # preprocess the image
+            x = model.forward(input_img)  # forward function of model
+
+        if valid_type == True:
+            """Save everything"""
+            save_path = os.path.join(net_save_dir, filename + ".npz")
+            feats = {}
+            for i, feat in enumerate(x):
+                if filetype == "jpg":
+                    feats[model.feat_list[i]] = feat.data.cpu().numpy()  # bring back to cpu
+
+            if not os.path.exists(net_save_dir):
+                os.makedirs(net_save_dir)
+            np.savez(save_path, **feats)  # save features
+
+            """Delete Cache"""
+            if torch.cuda.is_available():
+                torch.cuda.empty_cache()
+            del x
+            del feats
+
+
---
+++ matlab_rdms.py
@@ -0,0 +1,54 @@
+import scipy.io
+import glob
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import helper
+
+
+'''
+This file is used to convert and save the matlab files from RSA_Matrices directory as .npz files
+'''
+
+
+def visualize_rdm(rdm, npz_save_dir, filename):
+
+    number_of_matrix = 1
+    for matrix in rdm:
+
+        fig = plt.figure(figsize=(8, 6))
+        plt.imshow(matrix, cmap="inferno")
+        plt.title("Matrix of " + filename + "Number: " + str(number_of_matrix))
+        plt.colorbar()
+        save_path = os.path.join(npz_save_dir,filename + "Matrix " + str(number_of_matrix))
+        plt.savefig((save_path))
+        number_of_matrix += 1
+
+
+def read_in_rdms_matlab():
+    """Reads in all matlab files from RSA_Matrices and saves them as .npz files"""
+
+    path = "RSA_matrices"  # Path of the RSA matrices that evelyn has sent
+    mat_files = glob.glob(path + "/**/*.mat", recursive=True)
+
+    for mat_file in mat_files:
+        sep = helper.check_platform()  # depending on platform we have different seperators
+        filename = mat_file.split(sep)[-1].split(".")[0]
+        file_type = mat_file.split(sep)[-1].split(".")[1]
+        file_folder = mat_file.split(sep)[0]
+        npz_save_dir = file_folder + "\\" + filename
+        if file_type == "mat":
+            rdm_dict = scipy.io.loadmat(mat_file)
+            for key in rdm_dict:
+                if isinstance(rdm_dict[key], np.ndarray):
+                    if not os.path.exists(npz_save_dir):
+                        os.makedirs(npz_save_dir)
+                    save_path = os.path.join(npz_save_dir, filename + ".npz")
+                    rdm = rdm_dict[key]
+                    rdm2 = np.transpose(rdm, (2, 0, 1))    # Matrix reconfiguration 18x18x20 to 20x18x18
+                    np.savez(save_path, rdm2)  # save features
+                    visualize_rdm(rdm2, npz_save_dir, filename)
+
+
+"""This function only has to be executed once, it is not implemented into the main function"""
+read_in_rdms_matlab()
